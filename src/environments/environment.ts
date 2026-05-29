bcda07504ac4b7384ee4b50d3441735de09f69ff
luisdev20 <lescobedo.im@gmail.com>
2026-05-29 17:11:19 -0500

    Configurar URL final de Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU-API-EN-RENDER.onrender.com' // Cambiar a la URL pública final
+  apiUrl: 'https://naturavida-api.onrender.com'
 };
